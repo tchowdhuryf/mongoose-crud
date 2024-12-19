9e5ab9f7cf7dbfe303ecb5b8d147bbb432f8584a
tchowdhuryf <tasnim.chowdhury837@gmail.com>
2024-12-19 15:54:26 -0500

    added get single fruit and put routes

--- routes/fruitRoutes.js
+++ routes/fruitRoutes.js
@@ -16,11 +16,30 @@ router.get("/", async (req, res) => {
 router.post("/", async (req, res) => {
   try {
     const createdFruit = await Fruit.create(req.body);
-    console.log(req.body);
     res.json(createdFruit);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 });
+
+//show route - get 1 fruit
+router.get("/:id", async (req, res) => {
+  try {
+    const singleFruit = await Fruit.findById(req.params.id);
+    res.json(singleFruit);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+//edit route
+router.put("/:id", async (req, res) => {
+    try {
+      const updatedFruit = await Fruit.findByIdAndUpdate(req.params.id, req.body);
+    //   res.json(updatedFruit);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
 
 module.exports = router;
